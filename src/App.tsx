71fa89e85598fbc1f5eedc6fe0cc927a3886b2ca
mec-nyan <marianoraspberrypi@gmail.com>
2021-12-04 20:34:26 -0300

    Remove residual code

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,11 @@
-import './App.css'
-import { FaAngleDoubleLeft } from 'react-icons/fa'
+import "./App.css";
 
 function App() {
-
-  const next = () => console.log('NEXT')
-  const prev = () => console.log('PREVIOUS')
-
-  const n = e => console.log('PAGE', e.target.innerText)
-
   return (
     <div className="App">
-      <div 
-        id='myDiv1'
-        onClick={n}
-        style={{width:'100px',height:'100px',background:'red',fontSize:'60px'}}
-      >
-        1
-      </div>
-      <div 
-        id='myDiv1'
-        onClick={n}
-        style={{width:'100px',height:'100px',background:'magenta',fontSize:'60px'}}
-      >
-        2
-      </div>
-      <div 
-        id='myDiv1'
-        onClick={n}
-        style={{width:'100px',height:'100px',background:'green',fontSize:'60px'}}
-      >
-        3
-      </div>
-
-      <div 
-        id='myDiv1'
-        onClick={next}
-        style={{width:'100px',height:'100px',background:'red',fontSize:'60px'}}
-      >
-        <FaAngleDoubleLeft 
-          id='faangle'
-          style={{background:'white'}}
-        />
-      </div>
-
-      <div 
-        id='myDiv2'
-        onClick={prev}
-        style={{width:'100px',height:'100px',background:'blue',fontSize:'60px'}}
-      >
-        <FaAngleDoubleLeft 
-          id='faangle2'
-          style={{background:'white'}}
-        />
-      </div>
-
+      <h1>Mui tutorial</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
