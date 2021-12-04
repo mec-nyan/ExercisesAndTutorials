f4e0486e197c4c7079acbb71a85c9e9412ed891c
mec-nyan <marianoraspberrypi@gmail.com>
2021-12-04 20:59:12 -0300

    Material ui buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Mui tutorial</h1>
+      <Button variant="contained">A button</Button>
+      <Button>Button</Button>
     </div>
   );
 }
